d9d8bb9a2a4355a387dd0794999b3f3e1c7d3b54
BrunoRangel30 <rangel.bruno28@gmail.com>
2017-06-27 11:47:32 -0300

    insercao do mapa com shape Brasil

--- src/components/MapaSerie.js
+++ src/components/MapaSerie.js
@@ -1,44 +1,38 @@
 import React, { Component } from "react";
 import Choropleth from "react-leaflet-choropleth";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import chroma from 'chroma-js'
+import {statesData} from '../data/Coord.Mapa'
 
-class MapaSerie extends Component {
-  state = {
-    lat: 37.8,
-    lng: -96,
-    zoom: 4
-  };
-
-  render() {
-    const style = {
-      fillColor: "#F28F3B",
-      weight: 2,
-      opacity: 1,
-      color: "white",
-      dashArray: "3",
-      fillOpacity: 0.5
+        class MapaSerie extends Component {
+    state = {
+        lat: -15.7801,
+        lng: -47.9292,
+        zoom: 4,
     };
 
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution="&copy; <a href=&quot;http://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a>"
-          url="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-        />
-        {/*<Choropleth
-          data={{ type: "FeatureCollection", features: statesData }}
-          valueProperty={feature => feature.properties.value}
-          scale={["#b3cde0", "#011f4b"]}
-          steps={7}
-          mode="e"
-          style={style}
-          onEachFeature={(feature, layer) =>
-            layer.bindPopup(feature.properties.label)}
-        />*/}
-      </Map>
-    );
-  }
+    render() {
+        
+
+        const position = [this.state.lat, this.state.lng];
+        const geojson = statesData;
+        return (
+                <Map center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        attribution="&copy; <a href=&quot;http://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a>"
+                        url="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+                        />
+                    <Choropleth
+                        data={geojson}
+                        valueProperty={(feature) => feature.properties.density}
+                        steps={5}
+                        mode="q"
+                        onEachFeature={(feature, layer) => layer.bindPopup(feature.properties.name)}
+                        scale={['gray', 'red', 'green']}
+                        />
+                </Map>
+                );
+    }
 }
 
 export default MapaSerie;
